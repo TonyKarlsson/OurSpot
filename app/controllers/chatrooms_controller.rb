9912ca1c1b84bb0f452f94c749926428e8bdea43
Vasil Hodzhev <vasilhodzhev@gmail.com>
2020-11-25 12:12:10 +0100

    pundit implement

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,10 +1,12 @@
 class ChatroomsController < ApplicationController
 
   def index
+    authorize @chatroom
     @chatrooms = Chatroom.all
   end
 
   def show
+    authorize @chatroom
     @chatroom = Chatroom.find(params[:id])
   end
 
