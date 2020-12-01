5c966f64e50c8f00f2298f62b6fa915d3c132105
eternal17 <sarmadkhatri15@gmail.com>
2020-12-01 16:29:07 +0100

    changed to instance variable with correct name

--- app/services/user_in_message_radius.rb
+++ app/services/user_in_message_radius.rb
@@ -14,7 +14,7 @@ class UserInMessageRadius
   private
 
   def distance_between
-    Geocoder::Calculations.distance_between(current_user.current_sign_in_ip, "#{@message.latitude}, #{@message.longitude}")
+    Geocoder::Calculations.distance_between(@user.current_sign_in_ip, "#{@message.latitude}, #{@message.longitude}")
     # Geocoder::Calculations.distance_between("henriksdalsringen 27, Nacka, Stockholm", "#{@message.latitude}, #{@message.longitude}")
     # "59.3294, 18.0687"
   end
