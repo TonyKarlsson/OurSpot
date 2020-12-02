59efe09d46526eef14accdb9332b38a8afbea257
Tony Karlsson <tonykarlsson79@gmail.com>
2020-12-02 11:06:10 +0100

    Validations for username, coordinates, and friends in chatroom

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,6 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :messages
   belongs_to :friend1, class_name: "User"
   belongs_to :friend2, class_name: "User"
+  validates :friend2_id, :friend1_id, presence: true
+
   def recipient(user)
     self.friend2 == user ? friend1.username : friend2.username
   end
